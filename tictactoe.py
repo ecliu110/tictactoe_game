fdf601df82456c73d34a812b45897aa8af59bb93
ecliu110 <ellenliu94@gmail.com>
2017-06-15 19:55:01 -0500

    implemented win and tie check

--- tictactoe.py
+++ tictactoe.py
@@ -66,7 +66,7 @@ def get_game_status(game_id):
     game = GAMES.get(game_id)
     if not game:
         # This is an unknown game
-        return "Game not found", 404
+        return "Game not found\n", 404
 
     game = GAMES[game_id]
     return flask.jsonify(game)
@@ -88,11 +88,11 @@ def make_move(game_id):
     # Check if game exists
     game = GAMES.get(game_id)
     if not game:
-        return "Game not found", 404
+        return "Game not found\n", 404
 
     # Check if game is over
     if not game["status"] == "in progress":
-        return "This game is already over", 400
+        return "This game is already over\n", 400
 
     move = flask.request.get_json()  # Converts input data to JSON
     player = move["players_turn"]
@@ -113,8 +113,33 @@ def make_move(game_id):
     # Update the game's board
     game["board"][x][y] = player
     game["players_turn"] = "X" if (player == "Y") else "Y"
+
+    # check if game is over (tie/win)
+    if checkWin(game["board"], player):
+        game["status"] = player.lower() + " wins"
+        GAMES[game_id] = game
+        return flask.jsonify(game), 200
+
+    if isFull(game["board"]):
+        game["status"] = "tie"
+
     GAMES[game_id] = game
-
-     # TODO: check if game is over (tie/win)
-
     return flask.jsonify(game), 200
+
+def checkWin(board, player):
+    # check columns and rows
+    for i in range(0,3):
+        if(board[i][0] == board[i][1] == board[i][2] == player) or \
+        (board[0][i] == board[1][i] == board[2][i] == player):
+            return True
+    # check diagonals
+    if (board[0][0] == board[1][1] == board[2][2] == player) or \
+    (board[0][2] == board[1][1] == board[2][2] == player):
+        return True
+    return False
+
+def isFull(board):
+    for row in board:
+        if " " in row:
+            return False
+    return True
